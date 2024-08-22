e81cce0a5169b06ab074d20f76dc145433fa0931
serkansyalcin <serkan.syalcin@hotmail.com>
2024-08-22 16:38:59 +0300

    LangCheckerServiceProvider add.

---
+++ src/Providers/LangCheckerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TranslationCchecker\LangChecker\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LangCheckerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
